7c29da2126ef9618c70c41e0ad9549a96645a2ae
tentenxten <49960944+tentenxten@users.noreply.github.com>
2019-06-05 01:41:40 -0400

    Update server.spec.js

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,7 +1,7 @@
 var request = require('request');
 describe('get message',()=>{
     it('should return 200 ok',()=>{
-        request.get('http://localhost:3000/messages',(err,res)=>{
+        request.get('https://compassionate-jennings-1d9db6.netlify.com/messages',(err,res)=>{
             console.log(res);
             expect(res.statusCode).toEqual(200);
             done();
@@ -9,7 +9,7 @@ describe('get message',()=>{
     })
 
     it('should return a list, thats not empty',()=>{
-        request.get('http://localhost:3000/messages',(err,res)=>{
+        request.get('https://compassionate-jennings-1d9db6.netlify.com/messages',(err,res)=>{
             expect(res.body.length).toBeGreaterThan(0)
             done();
         })
@@ -18,14 +18,14 @@ describe('get message',()=>{
 
 describe('get message from user',()=>{
     it('should return 200 ok',()=>{
-        request.get('http://localhost:3000/messages/tim',(err,res)=>{
+        request.get('https://compassionate-jennings-1d9db6.netlify.com//messages/tim',(err,res)=>{
             console.log(res);
             expect(res.statusCode).toEqual(200);
             done();
         })
     })
     it('name should be tim',(done)=>{
-        request.get('http://localhost:3000/messages/tim',(err,res)=>{
+        request.get('https://compassionate-jennings-1d9db6.netlify.com//messages/tim',(err,res)=>{
             console.log(res);
             expect(JSON.parse(res.body)[0].name).toEqual('tim');
             done();
